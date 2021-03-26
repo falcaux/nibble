685eb6b8f4553af5d2e3da9068761d7c0a125ebb
falcaux <79866012+falcaux@users.noreply.github.com>
2021-03-26 11:54:11 +0100

    Update keymap.c

--- nibblevmedia/keymap.c
+++ nibblevmedia/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Jay Greco
+/* Copyright 2020 Jay Greco & Falcaux
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,6 +12,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *Include Copy and Past
+ *Include Media option
  */
 #include QMK_KEYBOARD_H
 
