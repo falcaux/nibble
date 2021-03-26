ddc096f868fd4a0a9fe426be98b77abbc41b5391
Falcaux <79866012+falcaux@users.noreply.github.com>
2021-03-26 14:35:18 +0100

    Update keymap.c
    Add FN1 and FN2 Options

--- keymaps/nibblevmedia/keymap.c
+++ keymaps/nibblevmedia/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Jay Greco & Falcaux
+/* Copyright 2020 Jay Greco
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,13 +12,13 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * Include Copy and Past 
- * Include Media option
+ */
 #include QMK_KEYBOARD_H
 
 enum layer_names {
   _MA,
-  _FN
+  _FN1,
+  _FN2
 };
 
 enum custom_keycodes {
@@ -31,14 +31,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F13, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
     KC_F14, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
     LCTL(KC_C), KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
-    LCTL(KC_Z), KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    KC_RALT, MO(_FN), KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT
+    LCTL(KC_Z), KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    KC_RALT, MO(_FN1), MO(_FN2), KC_LEFT,          KC_DOWN, KC_RGHT
   ),
-  [_FN] = LAYOUT_ansi(
+  
+  [_FN1] = LAYOUT_ansi(
                RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_END,
     RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_ASTR, KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_AT,            _______, KC_HOME,
     LCTL(KC_V), _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______,          _______, KC_END,
-    LCTL(KC_Y), _______, _______, _______,                   KC_MPLY,                   _______, _______, _______,          KC_MPRV, KC_MPLY, KC_MNXT),
+    LCTL(KC_Y), _______, _______, _______,                   KC_MPLY,                   _______, _______, _______,          KC_MPRV, KC_MPLY, KC_MNXT
+  ),
+  
+  [_FN2] = LAYOUT_ansi(
+               RESET,RGB_M_SN,RGB_M_SW,RGB_M_R,  RGB_M_P, RGB_M_X, RGB_M_B, RGB_M_K, RGB_M_G, BL_TOGG, BL_STEP, BL_BRTG, _______,  KC_DEL,  KC_END,
+    RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_ASTR, KC_DEL,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_AT,            _______, KC_HOME,
+    LCTL(KC_X), _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______,          _______, KC_END,
+    LCTL(KC_Y), _______, _______, _______,                   KC_MPLY,                   _______, _______, _______,          KC_MPRV, KC_MPLY, KC_MNXT
+  ),
 
 };
 
