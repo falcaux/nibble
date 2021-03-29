b3a5fab72c484bcd8ee20340a471f594af5236f2
Falcaux <79866012+falcaux@users.noreply.github.com>
2021-03-29 09:27:22 +0200

    Update keymap.c

--- keymaps/nibblevmedia/keymap.c
+++ keymaps/nibblevmedia/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Falcaux
+/* Copyright 2020 Jay Greco & Falcaux
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,7 +30,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MA] = LAYOUT_ansi(
             KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
-    KC_F13, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+    RGB_MOD, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
     KC_F14, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
     LCTL(KC_C), KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
     LCTL(KC_Z), KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    KC_RALT, MO(_FN1), MO(_FN2), KC_LEFT,          KC_DOWN, KC_RGHT
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   
   [_FN2] = LAYOUT_ansi(
-               RESET,RGB_M_SN,RGB_M_SW,RGB_M_R,  RGB_M_P, RGB_M_X, RGB_M_B, RGB_M_K, RGB_M_G, BL_TOGG, BL_STEP, BL_BRTG, _______,  KC_DEL,  KC_END,
+               RESET,RGB_M_SW,RGB_M_SN,RGB_M_K,  RGB_M_B, RGB_M_X, RGB_M_P, RGB_M_R, RGB_M_G, RGB_M_T, _______, _______, _______,  KC_DEL,  KC_END,
     RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_ASTR, KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_AT,            _______, KC_HOME,
     LCTL(KC_X), _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______,          _______, KC_END,
