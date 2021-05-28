570f9a504be352d60582b4b014177c0db22b1da2
Falcaux <79866012+falcaux@users.noreply.github.com>
2021-05-28 14:30:28 +0200

    Update nibble.c

--- nibble.c
+++ nibble.c
@@ -21,8 +21,19 @@
 // Use big_led LED to show CAPS LOCK status
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
+	bool red_enabled = 0;
+	bool green_enabled = 0;
+	bool blue_enabled = 0;
     if (res) {
-        set_big_LED_rgb(led_state.caps_lock ? LED_ON : LED_OFF, led_state.caps_lock ? LED_ON : LED_OFF, led_state.caps_lock ? LED_ON : LED_OFF);
+		if (led_state.caps_lock) {
+			red_enabled = rand()&1;
+			green_enabled = rand()&1;
+			blue_enabled = rand()&1;
+			if (green_enabled == 0 && red_enabled == 0 && blue_enabled == 0) {
+				green_enabled = 1;
+			}
+		}
+		set_big_LED_rgb(red_enabled ? LED_ON : LED_OFF, green_enabled ? LED_ON : LED_OFF, blue_enabled ? LED_ON : LED_OFF);		
     }
     return res;
 }
